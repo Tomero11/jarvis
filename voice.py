19454e3ecc82c0d219ffdafa5ec73bd291f7f7ad
Tomero11 <tomer12123333@gmail.com>
2026-07-17 08:33:10 +0300

    Handle voice connection errors

--- voice.py
+++ voice.py
@@ -3,15 +3,19 @@ import edge_tts
 from playsound3 import playsound
 
 VOICE = "he-IL-HilaNeural"
+VOICE_FILE = "jarvis_voice.mp3"
+
 
 def speak(text):
-
     async def _speak():
         await edge_tts.Communicate(
             text,
-            VOICE
-        ).save("jarvis_voice.mp3")
+            VOICE,
+        ).save(VOICE_FILE)
 
-    asyncio.run(_speak())
+    try:
+        asyncio.run(_speak())
+        playsound(VOICE_FILE)
 
-    playsound("jarvis_voice.mp3")
+    except Exception as e:
+        print(f"Voice Error: {e}")
